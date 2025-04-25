ba0a03ce50f052279295c2c3895324d23165c259
dvp-tae <th3016@naver.com>
2025-04-25 09:33:21 +0000

    sitemap robots 파일 추가

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://zaply.io";
+
+  const routes = ["", "/inquire"].map(route => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date(),
+    changeFrequency: route === "" ? "daily" : ("weekly" as "daily" | "weekly"),
+    priority: route === "" ? 1 : 0.8,
+  }));
+
+  return routes;
+}
