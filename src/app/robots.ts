210619f4906829ec1c4da572a3d94b43d5e5dea8
dvp-tae <th3016@naver.com>
2025-04-25 10:35:25 +0000

    .

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/private/", "/admin/"],
     },
-    sitemap: "https://zaply.io/sitemap.xml",
+    sitemap: "https://zaply-landing.vercel.app/sitemap.xml",
   };
 }
