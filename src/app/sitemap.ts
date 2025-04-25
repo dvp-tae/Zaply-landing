8f2d55ac60baa43922902f63d35f043e6b16ab03
dvp-tae <th3016@naver.com>
2025-04-25 10:33:29 +0000

    .

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://zaply-landing.vercel.app/";
+  const baseUrl = "https://zaply-landing.vercel.app";
 
   const routes = ["", "/inquire"].map(route => ({
     url: `${baseUrl}${route}`,
